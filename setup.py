a1cee84d9d4a106a8f95cfe29d161b68f655ec79
Anthony Christe <achriste@redvoxsound.com>
2021-05-11 12:13:44 -1000

    Change pandas requirement.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as requirements_file:
     requirements = list(filter(lambda line: (not line.startswith("#")) and len(line) > 0, requirements))
 
 setup(name="redvox-pandas",
-      version="0.0.2",
+      version="0.0.3",
       url='https://github.com/RedVoxInc/redpandas',
       license='Apache',
       author='RedVox',
