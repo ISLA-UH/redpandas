ca8b201a49b6080fa09e25b7417dceefc52f7841
meritxell <meritxell@isla.hawaii.edu>
2021-06-17 15:49:37 -1000

    moved old_config from archive

--- examples/skyfall/skyfall_config.py
+++ examples/skyfall/skyfall_config.py
@@ -8,7 +8,7 @@ Skyfall 2020 Configuration file
 
 # Step 0: I/O files
 # Absolute path to the skyfall data
-# INPUT_DIR = "/Users/mgarces/Documents/DATA/SDK_DATA/api900_Skyfall_20201027"
+INPUT_DIR = "/Users/mgarces/Documents/DATA/SDK_DATA/api900_Skyfall_20201027"
 
 # See above for example of a well named directory
 # Avoid specifying /api900 directories if possible.
@@ -18,17 +18,17 @@ Skyfall 2020 Configuration file
 # INPUT_DIR = "/Users/tokyok/Desktop/skyfall"
 # INPUT_DIR = "/Users/meritxell/Documents/skyfall"
 
-# RPD_DIR = "rpd_files"
-# EVENT_NAME = "Skyfall"
+RPD_DIR = "rpd_files"
+EVENT_NAME = "Skyfall"
 
 # Absolute path for output pickle and parquet files
-# OUTPUT_DIR = os.path.join(INPUT_DIR, RPD_DIR)
+OUTPUT_DIR = os.path.join(INPUT_DIR, RPD_DIR)
 
 # Data Window Pickle
-# DW_FILE = EVENT_NAME  # + ".pickle"
+DW_FILE = EVENT_NAME  # + ".pickle"
 
 # RedPandas Parquets
-# PD_PQT_FILE = EVENT_NAME + "_df.parquet"
+PD_PQT_FILE = EVENT_NAME + "_df.parquet"
 
 # Absolute path to bounder data; could be a list
 OTHER_INPUT_PATH = os.path.join(INPUT_DIR, "bounder")
@@ -36,24 +36,24 @@ OTHER_INPUT_FILE = "skyfall_bounder.csv"
 OTHER_PD_PQT_FILE = EVENT_NAME + "_df_bounder.parquet"
 
 # Step 1: Station ID and Event time
-# STATIONS = ["1637610021"]
-# Timestamps in seconds since UTC epoch
-# EVENT_ORIGIN_EPOCH_S = 1603806314  # 2020-10-27T13:45:14
-# duration_s = 30*60  # 30 minutes
+STATIONS = ["1637610021"]
+Timestamps in seconds since UTC epoch
+EVENT_ORIGIN_EPOCH_S = 1603806314  # 2020-10-27T13:45:14
+duration_s = 30*60  # 30 minutes
 
 # From start
-# EPISODE_START_EPOCH_S = EVENT_ORIGIN_EPOCH_S
-# EPISODE_END_EPOCH_S = EVENT_ORIGIN_EPOCH_S + duration_s
+EPISODE_START_EPOCH_S = EVENT_ORIGIN_EPOCH_S
+EPISODE_END_EPOCH_S = EVENT_ORIGIN_EPOCH_S + duration_s
 
 # Reference lat lon altitude and time at terminus, from Bounder
-# ref_latitude_deg = 35.83728
-# ref_longitude_deg = -115.57234
-# ref_altitude_m = 1028.2
-# ref_epoch_s = 1603808160
+ref_latitude_deg = 35.83728
+ref_longitude_deg = -115.57234
+ref_altitude_m = 1028.2
+ref_epoch_s = 1603808160
 
 # Step 2: Sensor selection
-# SENSOR_LABEL = ['audio', 'barometer', 'accelerometer', 'magnetometer', 'gyroscope',
-#                 'health', 'location', 'clock', 'synchronization']
+SENSOR_LABEL = ['audio', 'barometer', 'accelerometer', 'magnetometer', 'gyroscope',
+                'health', 'location', 'clock', 'synchronization']
 
 # Step 3: Pipeline actions, needed parquet for geospatial
 build_dw_pickle: bool = False  # Handling of RDVX DataWindow structure
@@ -80,7 +80,6 @@ use_parquet: bool = True  # Load data using parquet with RedPandas dataframe
 # Build Bounder Data Products: Settings for skyfall_loc_rpd.py
 is_rerun_bounder: bool = True  # If true, rerun and save as parquet
 
-PIPELINE_LABEL = ['TBD']
 
 
 
