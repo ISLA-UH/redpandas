3f01942383451bbe909de02aa90de958f3333e63
meritxell <meritxell@isla.hawaii.edu>
2021-07-21 16:54:08 -1000

    fix broken tdr

--- examples/skyfall/skyfall_tdr_rpd.py
+++ examples/skyfall/skyfall_tdr_rpd.py
@@ -424,9 +424,6 @@ def main():
                                           accelerometer_epoch_s_label, gyroscope_epoch_s_label,
                                           magnetometer_epoch_s_label]
 
-        sensor_sample_rate_column_label_list = [audio_fs_label, barometer_fs_label, accelerometer_fs_label,
-                                                gyroscope_fs_label, magnetometer_fs_label]
-
         sensor_ticklabels_list = ['Audio', 'Bar hp', 'Acc X hp', 'Acc Y hp',
                                   'Acc Z hp', 'Gyr X hp', 'Gyr Y hp', 'Gyr Z hp',
                                   'Mag X hp', 'Mag Y hp', 'Mag Z hp']
@@ -445,12 +442,12 @@ def main():
 
         rpd_plot.plot_wiggles_pandas(df=df_skyfall_data,
                                      sig_wf_label=sensor_column_label_list,
-                                     sig_sample_rate_label=sensor_sample_rate_column_label_list,
+                                     sig_timestamps_label=sensor_epoch_column_label_list,
                                      sig_id_label='station_id',
                                      station_id_str='1637610021',
                                      x_label="Time (s)",
                                      y_label='Sensor',
-                                     fig_title_show=False,
+                                     fig_title_show=True,
                                      fig_title='sensor waveforms',
                                      wf_color='midnightblue',
                                      custom_yticks=sensor_ticklabels_list)
