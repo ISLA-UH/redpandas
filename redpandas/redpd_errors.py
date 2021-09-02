728e4756dd87bb875339829a7d784325876ac2c5
meritxell <meritxell@isla.hawaii.edu>
2021-09-01 17:04:09 -1000

    redpandas errors, mesh changes

--- redpandas/redpd_errors.py
+++ redpandas/redpd_errors.py
@@ -1,3 +1,22 @@
 """
 Module for custom RedPandas errors
 """
+
+
+class RedPandasError(Exception):
+    """
+    Base RedPandas errors class
+    """
+    def __init__(self, message: str):
+        super().__init__(f"RedPandasError: {message}")
+
+
+class ValueMismatch(RedPandasError):
+
+    def __init__(self, message: str):
+        super().__init__(f"V: {message}")
+
+
+# if __name__ == "__main__":
+#
+#     raise ValueMismatch("d")
